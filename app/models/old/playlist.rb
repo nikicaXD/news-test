a7ae60f6c72f08b2000617c2216c329e86e5cef9
Uroš Gabrovec <uros.gabrovec@gmail.com>
2020-05-19 04:44:47 +0200

    popravlen_grd

--- app/models/old/playlist.rb
+++
@@ -1,6 +0,0 @@
-class Playlist < ApplicationRecord
-    belongs_to :user
-    has_many :playlist_posts
-    has_many :posts, through: :playlist_posts
-    # has_and_belongs_to_many :posts
-end
--- app/models/old/playlist_song.rb
+++
@@ -1,4 +0,0 @@
-class PlaylistPost < ApplicationRecord
-  belongs_to :playlist
-  belongs_to :post
-end
